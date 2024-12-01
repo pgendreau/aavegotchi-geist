1fd75b4ab9a1323b38df386b4f371628b35c9d08
Coderdan <dmathieuva@gmail.com>
2024-12-01 14:46:41 +0800

    Update upgrade-wearableDiamond.ts

--- scripts/upgrades/upgrade-wearableDiamond.ts
+++ scripts/upgrades/upgrade-wearableDiamond.ts
@@ -31,26 +31,26 @@ export async function upgrade() {
 
   //now try running a bridge
 
-  const itemBridgeAddress = bridgeConfig[63157]["GOTCHI_ITEM"].MintableToken;
+  // const itemBridgeAddress = bridgeConfig[63157]["GOTCHI_ITEM"].MintableToken;
 
-  const impersonatedSigner = await ethers.getImpersonatedSigner(
-    "0xC3c2e1Cf099Bc6e1fA94ce358562BCbD5cc59FE5"
-  );
+  // const impersonatedSigner = await ethers.getImpersonatedSigner(
+  //   "0xC3c2e1Cf099Bc6e1fA94ce358562BCbD5cc59FE5"
+  // );
 
-  const signer = await (await ethers.getSigners())[0];
-  const itemBridge = await ethers.getContractAt(
-    "WearablesFacet",
-    itemBridgeAddress,
-    impersonatedSigner
-  );
+  // const signer = await (await ethers.getSigners())[0];
+  // const itemBridge = await ethers.getContractAt(
+  //   "WearablesFacet",
+  //   itemBridgeAddress,
+  //   impersonatedSigner
+  // );
 
-  const tx = await itemBridge.bridgeItem(
-    "0xC3c2e1Cf099Bc6e1fA94ce358562BCbD5cc59FE5",
-    "228",
-    "1",
-    500_000,
-    bridgeConfig[63157]["GOTCHI_ITEM"].connectors["137"].FAST
-  );
+  // const tx = await itemBridge.bridgeItem(
+  //   "0xC3c2e1Cf099Bc6e1fA94ce358562BCbD5cc59FE5",
+  //   "228",
+  //   "1",
+  //   500_000,
+  //   bridgeConfig[63157]["GOTCHI_ITEM"].connectors["137"].FAST
+  // );
 }
 
 if (require.main === module) {
