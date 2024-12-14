63db4fdeaba0448fafe436ba8fb4c6f2c4a3b7d6
pgendreau <pgendreau@gmail.com>
2024-12-14 16:41:52 -0500

    add sanity checks

--- contracts/Aavegotchi/facets/WearablesConfigFacet.sol
+++ contracts/Aavegotchi/facets/WearablesConfigFacet.sol
@@ -3,6 +3,7 @@ pragma solidity 0.8.1;
 
 import {LibMeta} from "../../shared/libraries/LibMeta.sol";
 import {Modifiers, AavegotchiWearableConfigs, WearablesConfig, EQUIPPED_WEARABLE_SLOTS} from "../libraries/LibAppStorage.sol";
+import {LibAavegotchi} from "../libraries/LibAavegotchi.sol";
 import {LibWearablesConfig} from "../libraries/LibWearablesConfig.sol";
 
 contract WearablesConfigFacet is Modifiers {
@@ -10,32 +11,44 @@ contract WearablesConfigFacet is Modifiers {
     uint8 public constant WEARABLESCONFIG_FREE_SLOTS = 3;
     uint256 public constant WEARABLESCONFIG_SLOT_PRICE = 1000000000000000000; // 1 GHST
 
-    event WearablesConfigCreated(address indexed owner, uint256 indexed tokenId, uint256 indexed slot, uint256[EQUIPPED_WEARABLE_SLOTS] wearables, uint256 value);
-    event WearablesConfigUpdated(address indexed owner, uint256 indexed tokenId, uint256 indexed slot, uint256[EQUIPPED_WEARABLE_SLOTS] wearables);
+    event WearablesConfigCreated(address indexed owner, uint256 indexed tokenId, uint256 indexed wearablesConfigId, uint256[EQUIPPED_WEARABLE_SLOTS] wearables, uint256 value);
+    event WearablesConfigUpdated(address indexed owner, uint256 indexed tokenId, uint256 indexed wearablesConfigId, uint256[EQUIPPED_WEARABLE_SLOTS] wearables);
     event WearablesConfigPaymentReceived(address indexed owner, uint256 indexed tokenId, uint256 indexed wearablesConfigId, uint256 value);
 
-    function createWearablesConfig(uint256 _tokenId, string calldata _name, uint256[EQUIPPED_WEARABLE_SLOTS] calldata _wearablesToStore) external payable returns (uint256 wearablesConfigId) {
+    function createWearablesConfig(
+        uint256 _tokenId,
+        string calldata _name,
+        uint256[EQUIPPED_WEARABLE_SLOTS] calldata _wearablesToStore
+    )
+        external
+        payable
+        onlyAavegotchiOwner(_tokenId)
+        returns (uint256 wearablesConfigId)
+    {
         // check that tokenId is a gotchi
-        // check that msg.sender is gotchi owner (owned or rented)
-        // check that wearables are valid and for the right slots
+        require(s.aavegotchis[_tokenId].status == LibAavegotchi.STATUS_AAVEGOTCHI, "WearablesConfigFacet: Can only create wearables config for Aavegotchi");
+        require(LibWearablesConfig._checkValidWearables(_wearablesToStore), "WearablesConfigFacet: Invalid wearables");
         require(bytes(_name).length > 0, "WearablesConfigFacet: WearablesConfig name cannot be blank");
 
         address owner = LibMeta.msgSender();
-
-        if (s.ownersWearableConfigs[owner][_tokenId].slotsUsed >= WEARABLESCONFIG_FREE_SLOTS) {
-            require(msg.value == WEARABLESCONFIG_SLOT_PRICE, "WearablesConfigFacet: Incorrect GHST value sent");
-
-            (bool success, ) = payable(s.daoTreasury).call{value: msg.value}("");
-            require(success, "WearablesConfigFacet: Failed to send ETH to DAO treasury");
-        }
-
         uint256 wearablesConfigId = LibWearablesConfig._getNewWearablesConfigId(owner, _tokenId);
-        WearablesConfig memory wearablesConfig = WearablesConfig({name: _name, wearables: _wearablesToStore});
-
-        s.ownersWearableConfigs[owner][_tokenId].wearableConfigs.push(wearablesConfig);
 
         // increment slots used
         s.ownersWearableConfigs[owner][_tokenId].slotsUsed += 1;
+
+        // if the ownner has reached the free slots limit then they need to pay for the extra slot
+        if (wearablesConfigId > WEARABLESCONFIG_FREE_SLOTS) {
+            require(msg.value == WEARABLESCONFIG_SLOT_PRICE, "WearablesConfigFacet: Incorrect GHST value sent");
+
+            // send GHST to the dao treasury
+            (bool success, ) = payable(s.daoTreasury).call{value: msg.value}("");
+            require(success, "WearablesConfigFacet: Failed to send ETH to DAO treasury");
+
+            emit WearablesConfigPaymentReceived(owner, _tokenId, wearablesConfigId, msg.value);
+        }
+
+        WearablesConfig memory wearablesConfig = WearablesConfig({name: _name, wearables: _wearablesToStore});
+        s.ownersWearableConfigs[owner][_tokenId].wearableConfigs.push(wearablesConfig);
 
         emit WearablesConfigCreated(owner, _tokenId, wearablesConfigId, _wearablesToStore, msg.value);
 
@@ -43,7 +56,14 @@ contract WearablesConfigFacet is Modifiers {
     }
 
     /// @dev if _name is empty, name is not updated
-    function updateWearablesConfig(uint256 _tokenId, uint256 _wearablesConfigId, string calldata _name, uint256[EQUIPPED_WEARABLE_SLOTS] calldata _wearablesToStore) external {
+    function updateWearablesConfig(
+        uint256 _tokenId,
+        uint256 _wearablesConfigId,
+        string calldata _name,
+        uint256[EQUIPPED_WEARABLE_SLOTS] calldata _wearablesToStore
+    )
+        external
+    {
         address owner = LibMeta.msgSender();
 
         // check that wearables are valid and for the right slots
--- contracts/Aavegotchi/libraries/LibWearablesConfig.sol
+++ contracts/Aavegotchi/libraries/LibWearablesConfig.sol
@@ -2,13 +2,15 @@
 pragma solidity 0.8.1;
 
 import {LibMeta} from "../../shared/libraries/LibMeta.sol";
-import {LibAppStorage, AppStorage, WearablesConfig} from "../libraries/LibAppStorage.sol";
+import {LibItems} from "../libraries/LibItems.sol";
+import {LibAppStorage, AppStorage, WearablesConfig, ItemType, EQUIPPED_WEARABLE_SLOTS} from "../libraries/LibAppStorage.sol";
 
 library LibWearablesConfig {
 
     function _getNewWearablesConfigId(address _owner, uint256 _tokenId) internal view returns (uint256 wearablesConfigId) {
         AppStorage storage s = LibAppStorage.diamondStorage();
-        wearablesConfigId = uint256(s.ownersWearableConfigs[_owner][_tokenId].slotsUsed + 1);
+        // slots start at 0 so last slot id is slotsUsed - 1
+        wearablesConfigId = uint256(s.ownersWearableConfigs[_owner][_tokenId].slotsUsed);
     }
 
     function _wearablesConfigExists(address _owner, uint256 _tokenId, uint256 _wearablesConfigId) internal view returns (bool exists) {
@@ -16,4 +18,24 @@ library LibWearablesConfig {
         exists = (s.ownersWearableConfigs[_owner][_tokenId].slotsUsed >= _wearablesConfigId);
     }
 
-} 
+    function _checkValidWearables(uint256[EQUIPPED_WEARABLE_SLOTS] memory _wearablesToStore) internal view returns (bool valid) {
+        bool valid = true;
+        AppStorage storage s = LibAppStorage.diamondStorage();
+        for (uint256 slot; slot < EQUIPPED_WEARABLE_SLOTS; slot++) {
+            uint256 toStoreId = _wearablesToStore[slot];
+
+            if (toStoreId != 0) {
+                ItemType storage itemType = s.itemTypes[toStoreId];
+                if (itemType.category != LibItems.ITEM_CATEGORY_WEARABLE) {
+                  valid = false;
+                  break;
+                }
+                if (itemType.slotPositions[slot] == false) {
+                  valid = false;
+                  break;
+                }
+            }
+        }
+        return valid;
+    }
+}
