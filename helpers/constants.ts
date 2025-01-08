b6a31d3ea981f3597640512ed6ba512a402987e3
Coderdan <dmathieuva@gmail.com>
2025-01-08 20:07:23 +0800

    Update constants.ts

--- helpers/constants.ts
+++ helpers/constants.ts
@@ -76,6 +76,7 @@ export const AMOY_FORGE_DIAMOND = "0xF7c2AC46723Ad844620F798ECe67f5C673120FB6";
 interface NetworkAddresses {
   wghst: string;
   ghst?: string;
+  aavegotchiDiamond?: string;
 }
 
 export const networkAddresses: Record<number, NetworkAddresses> = {
@@ -90,6 +91,7 @@ export const networkAddresses: Record<number, NetworkAddresses> = {
   },
   63157: {
     wghst: "0x74e2051A9Cd994F83f2d789448dCa4a3e879964c",
+    aavegotchiDiamond: "0x6Acc828BbbC6874de40Ca20bfeA7Cd2a2DA8DA8c",
   },
   84532: {
     wghst: "0xe97f36a00058aa7dfc4e85d23532c3f70453a7ae",
